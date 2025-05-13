2576f4d1c341aef826e7fe06cd306ffd86e19099
amir fenina <109026398+amirf95@users.noreply.github.com>
2025-05-13 22:05:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import cv2 
+import numpy as np
+import streamlit as st
+from tensorflow.keras.applications.mobilenet_v2 import (
+    MobileNetV2,
+    preprocess_input,
+    decode_predictions
+)
+
+from PIL import Image
+
+def load_model():
+    model = MobileNetV2(weights='imagenet')
+    return model
+def preprocess_image(image):
+    img=np.array(image) # Convert PIL image to numpy array
+    img = cv2.resize(img, (224, 224))# Resize image to 224x224
+    img = np.expand_dims(img, axis=0)# Add batch dimension
+    img = preprocess_input(img)# Preprocess the image
+    return img
+def classify_image(model, image):
+   try:
+       processed_image=preprocess_image(image)
+       predictions = model.predict(processed_image)
+       decoded_predictions = decode_predictions(predictions, top=3)[0]
+    #    Display the top 3 predictions
+       return decoded_predictions
+   except Exception as e:
+        st.error(f"Error in classification: {e}")
+        return None
+def main():
+    st.set_page_config(page_title="Image Classification App", page_icon="🤖", layout="centered")
+
+    st.title("Image Classification App")
+    st.write("Upload an image to classify it using MobileNetV2.")
+
+# Load the model do not doing it every time
+    @st.cache_resource
+    def load_cached_model():
+        return load_model()
+    
+    model = load_cached_model()
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        image = st.image(uploaded_file, caption='Uploaded Image', use_container_width=True)
+        
+        
+        btn = st.button("Classify Image")
+        if btn:
+             with st.spinner("Classifying..."):
+                  image = Image.open(uploaded_file)
+                  predictions= classify_image(model, image)
+                  if predictions:
+                        st.subheader("Predictions:")
+                        for _ , label, score in predictions:
+                            st.write(f"{label}: {score:.2%}")
+    
+if __name__ == "__main__":
+    main()
+# Run the app using streamlit run main.py
+
+       
